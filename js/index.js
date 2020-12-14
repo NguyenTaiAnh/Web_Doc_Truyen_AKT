1d2af2f27833908e12f92b241f10e2dcbcf693f8
akonho99qn <akonho99qn@gmail.com>
2020-12-14 15:22:26 +0700

    fix slogan

--- js/index.js
+++ js/index.js
@@ -18,3 +18,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    Typed.new('.type', {
+        strings: ["Đọc Truyện Không Hay Xóa Web Truyện"],
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 60,
+        // time before typing starts
+        startDelay: 600,
+        // backspacing speed
+        backSpeed: 20,
+        // time before backspacing
+        backDelay: 500,
+        // loop
+        loop: true,
+        // false = infinite
+        loopCount: 5,
+        // show cursor
+        showCursor: false,
+        // character for cursor
+        cursorChar: "|",
+        // attribute to type (null == text)
+        attr: null,
+        // either html or text
+        contentType: 'html',
+    });
+});
